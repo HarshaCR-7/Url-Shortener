bb746f9989f261f10ce6c848c88911b41c32ba6f
HarshaCR-7 <harshacr007@gmail.com>
2021-12-20 15:30:03 +0530

    update

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -5,18 +5,14 @@ from .models import db, User
 from .routes import views
 from .auth import auth
 from flask_restful import Api
-from flask_jwt_extended import JWTManager
 
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
     api =   Api(app)
-    app.config.from_pyfile(config_file)
-    app.secret_key = 'development'
-    app.config['JWT_SECRET_KEY'] = 'JWT_SECRET_KEY'
+    app.config['SECRET_KEY'] = 'hjshjhdjahjfjfjkjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///db.sqlite3'
     db.init_app(app)
-    JWTManager(app)
 
-    db.init_app(app)
 
     app.register_blueprint(views)
     app.register_blueprint(auth)
