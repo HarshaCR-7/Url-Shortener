b0f502a6270ea3d0267b298f9d8f014066183ee2
HarshaCR-7 <harshacr007@gmail.com>
2021-12-21 13:36:28 +0530

    update

--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -2,14 +2,14 @@ import re
 from flask import Blueprint, render_template,redirect,request
 from flask.helpers import flash
 from .models import db
-
+from flask_login import login_user, login_required, logout_user, current_user
 from .models import Link,User
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html',user=current_user) 
 
 
 @views.route('/add_link', methods=['GET','POST'])
@@ -25,7 +25,7 @@ def add_link():
             new_link=link.short_url, original_url=link.original_url)
     return render_template('link_added.html')
 
-from flask_login import login_user, login_required, logout_user, current_user
+
 @views.route('/<short_url>')
 def redirect_to_url(short_url):
     link = Link.query.filter_by(short_url=short_url).first_or_404()
@@ -44,13 +44,13 @@ def stats():
 @views.route('/add', methods = ['GET','POST'])
 @login_required
 def add():
-    links = Link.query.all() 	
+    links = Link.query.filter_by(user_id=current_user.id).order_by(Link.id.desc()).all() 	
     if request.method == 'POST':
         full_url = request.form.getlist('field[]')
         n = request.form['limit']
         i = 1
-        str = 'http://127.0.0.1:8000/rss?f='
-        str1 = ''
+        #str = 'http://127.0.0.1:8000/rss?f='
+        str = 'https://feed-mixer.herokuapp.com/rss?f='
         for value in full_url:
             if value != '':
                 if i == 1:
@@ -72,6 +72,6 @@ def add():
         result = Link.query.filter_by(original_url=str).first_or_404()
         links = Link.query.filter_by(user_id=current_user.id).all()
         
-        return render_template('url_add.html',links=links,user=current_user)
+        return render_template('link_added.html',original_url=result.original_url,new_link=result.short_url,user=current_user)
     links = Link.query.filter_by(user_id=current_user.id).all()
     return render_template('url_add.html',user=current_user,links=links)
