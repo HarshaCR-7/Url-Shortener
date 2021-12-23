2491de1efd81b7c27c777fb9ab588510b9cadfb4
HarshaCR-7 <harshacr007@gmail.com>
2021-12-23 17:18:32 +0530

    update

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask, app
 
-from .api import initialize_routes
 from .models import db, User
 from .routes import views
 from .auth import auth
-from flask_restful import Api
 
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
-    api =   Api(app)
     app.config['SECRET_KEY'] = 'hjshjhdjahjfjfjkjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///db.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -25,7 +22,5 @@ def create_app(config_file='settings.py'):
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-
-    initialize_routes(api)
 
     return app
