65f44b59d06b42c9501b5ea53aa8eabe3bf6e181
HarshaCR-7 <harshacr007@gmail.com>
2021-12-24 15:40:39 +0530

    Postgres

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -10,18 +10,18 @@ def create_app(config_file='settings.py'):
         return render_template('404.html')
     app.config['SECRET_KEY'] = 'hjshjhdjahjfjfjkjshkjdhjs'
     
-    #ENV = 'Prod'  #For Production porpose
-    ENV = 'dev'  #local For Development porpose
+    ENV = 'Prod'  #For Production porpose
+    #ENV = 'dev'  #local For Development porpose
 
     if ENV == 'dev':  #local For Development porpose
         app.debug = True
         #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/mixer'
-        #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///db.sqlite3'
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://csvwriuijpgbki:f00571a2992db6f219b352125c7c87633baf0e1de5999a900210ad6b1f6829c7@ec2-50-19-160-40.compute-1.amazonaws.com:5432/d36uf0phsvofer'
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///db.sqlite3'
+        #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://csvwriuijpgbki:f00571a2992db6f219b352125c7c87633baf0e1de5999a900210ad6b1f6829c7@ec2-50-19-160-40.compute-1.amazonaws.com:5432/d36uf0phsvofer'
     else:  #For Production porpose
         app.debug = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///db.sqlite3'
-        #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://csvwriuijpgbki:f00571a2992db6f219b352125c7c87633baf0e1de5999a900210ad6b1f6829c7@ec2-50-19-160-40.compute-1.amazonaws.com:5432/d36uf0phsvofer'
+        #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///db.sqlite3'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://csvwriuijpgbki:f00571a2992db6f219b352125c7c87633baf0e1de5999a900210ad6b1f6829c7@ec2-50-19-160-40.compute-1.amazonaws.com:5432/d36uf0phsvofer'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
